9ca8eaf0fed5fde7b5ed95dadd1437030c6d6a26
Mukul Narwani <narwani.m@husky.neu.edu>
2019-10-23 16:52:24 -0400

    Add ProofRead tab with multiple textfields

--- src/neuroglancer/user_layer_with_proofread.ts
+++ src/neuroglancer/user_layer_with_proofread.ts
@@ -20,10 +20,16 @@ import {ProofreadTab} from 'neuroglancer/widget/proofread_tab';
 
 
 const NEURON_NAME_JSON_KEY = 'proofreadNeuronName';
+const TAGS_JSON_KEY = 'tags';
+const LOC_TAGS_JSON_KEY = 'loc_tags';
+const ANNOTATOR_JSON_KEY = 'annotator';
 const PROOFREAD_TAB_NAME = 'Proofread'
 
 export interface UserLayerWithProofread extends UserLayer {
     neuronName: TrackableString;
+    tags: TrackableString;
+    loc_tags: TrackableString;
+    annotator: TrackableString;
 }
 
 /**
@@ -33,22 +39,35 @@ export function UserLayerWithProofreadMixin<TBase extends {new (...args: any[]):
     Base: TBase) {
   class C extends Base implements UserLayerWithProofread {
     neuronName = new TrackableString();
+    tags = new TrackableString();
+    loc_tags = new TrackableString();
+    annotator = new TrackableString();
 
     constructor(...args: any[]) {
       super(...args);
       this.neuronName.changed.add(this.specificationChanged.dispatch);
+      this.tags.changed.add(this.specificationChanged.dispatch);
+      this.loc_tags.changed.add(this.specificationChanged.dispatch);
+      this.annotator.changed.add(this.specificationChanged.dispatch);
+
       this.tabs.add(PROOFREAD_TAB_NAME, {
         label: PROOFREAD_TAB_NAME,
         order: 100,
-        getter: () => new ProofreadTab(this.neuronName)
+        getter: () => new ProofreadTab(this.neuronName, this.tags,this.loc_tags,this.annotator)
       });
       const specification = args[1];
       this.neuronName.restoreState(specification[NEURON_NAME_JSON_KEY]);
+      this.tags.restoreState(specification[TAGS_JSON_KEY])
+      this.loc_tags.restoreState(specification[TAGS_JSON_KEY])
+      this.annotator.restoreState(specification[TAGS_JSON_KEY])
     }
 
     toJSON(): any {
       const x = super.toJSON();
       x[NEURON_NAME_JSON_KEY] = this.neuronName.toJSON();
+      x[TAGS_JSON_KEY]= this.tags.toJSON();
+      x[LOC_TAGS_JSON_KEY]=this.loc_tags.toJSON();
+      x[ANNOTATOR_JSON_KEY]=this.annotator.toJSON();
       return x;
     }
   }
--- src/neuroglancer/widget/proofread_tab.ts
+++ src/neuroglancer/widget/proofread_tab.ts
@@ -24,50 +24,135 @@ import {TrackableString} from 'neuroglancer/trackable_string';
 import {Tab} from 'neuroglancer/widget/tab_view';
 
 export class ProofreadTab extends Tab {
-  private textArea = document.createElement('textarea');
+  private prNeuronName = document.createElement('textarea');
+  private prTags = document.createElement('textarea');
+  private prLocTags = document.createElement('textarea');
+  private prAnnotator = document.createElement('textarea');
   
-  constructor(public transform: TrackableString) {
+
+  
+  constructor(public transform: TrackableString,public t: TrackableString,public lt: TrackableString,public a: TrackableString) {
     super();
     const {element} = this;
     element.classList.add('neuroglancer-Proofread-widget');
-    const {textArea} = this;
-    const textAreaLabel = document.createElement('label');
-    textAreaLabel.className = 'neuroglancer-Proofread-homogeneous';
-    //textAreaLabel.textContent = 'BUTTON';
-    textAreaLabel.appendChild(textArea);
-    element.appendChild(textAreaLabel);
+
+    const div_prNeuronName = document.createElement('DIV');
+    const {prNeuronName} = this;
+    const Neuron_names = document.createElement("H3");
+    Neuron_names.appendChild(document.createTextNode("Neuron_names"));
+    const prNeuronNameLabel = document.createElement('label');
+    prNeuronNameLabel.className = 'neuroglancer-Proofread-homogeneous';
+    //prNeuronNameLabel.textContent = 'Neuron name';
+    prNeuronNameLabel.appendChild(prNeuronName);
+
+    div_prNeuronName.appendChild(Neuron_names);
+    div_prNeuronName.appendChild(prNeuronNameLabel);
+    
+    element.appendChild(div_prNeuronName);
     this.registerDisposer(transform.changed.add(() => this.updateView()));
     this.registerDisposer(this.visibility.changed.add(() => this.updateView()));
-    textArea.addEventListener('save', () => this.updateModel());
-    textArea.addEventListener('blur', () => this.updateModel());
+    prNeuronName.addEventListener('save', () => this.updateModel());
+    prNeuronName.addEventListener('blur', () => this.updateModel());
+    prNeuronName.title = 'prNeuronName';
+    prNeuronName.rows = 1;
+
+
+    const div_prTags = document.createElement('DIV');
+    const {prTags} = this;
+    const tags = document.createElement("H3");
+    tags.appendChild(document.createTextNode("Tags"));
+    const prTagsLabel = document.createElement('label');
+    prTagsLabel.className = 'neuroglancer-Proofread-homogeneous';
+    //prTagsLabel.textContent = 'Tags';
+    prTagsLabel.appendChild(prTags);
+
+    div_prTags.appendChild(tags);
+    div_prTags.appendChild(prTagsLabel);
+    
+    element.appendChild(div_prTags);
+    this.registerDisposer(t.changed.add(() => this.updateView()));
+    this.registerDisposer(this.visibility.changed.add(() => this.updateView()));
+    prTags.addEventListener('save', () => this.updateModel());
+    prTags.addEventListener('blur', () => this.updateModel());
+    prTags.title = 'prTags';
+    prTags.rows = 3;
     
 
-    textArea.title = 'GUI_TextArea';
-    textArea.rows = 1;
-    //const saveButton = document.createElement('button');
-    //saveButton.textContent = 'Save';
-    //saveButton.addEventListener('click', () => this.updateModel());
-    //element.appendChild(saveButton);
+    const div_prLocTags = document.createElement('DIV');
+    const {prLocTags} = this;
+    const prLocTagsLabel = document.createElement('label');
+    const Location_tags = document.createElement("H3");
+    const linebreak = document.createElement("br");
+    Location_tags.appendChild(document.createTextNode("Location_tags"));
+    prLocTagsLabel.className = 'neuroglancer-Proofread-homogeneous';
+    //prLocTagsLabel.textContent = 'Location tags';
+    prLocTagsLabel.appendChild(prLocTags);
 
-
-    const resetButton = document.createElement('button');
+    div_prLocTags.appendChild(Location_tags);
+    div_prLocTags.appendChild(prLocTagsLabel);
+    //div_prLocTags.appendChild(linebreak);
+    element.appendChild(div_prLocTags);
+    this.registerDisposer(lt.changed.add(() => this.updateView()));
+    this.registerDisposer(this.visibility.changed.add(() => this.updateView()));
+    prLocTags.addEventListener('save', () => this.updateModel());
+    prLocTags.addEventListener('blur', () => this.updateModel());
+    prLocTags.title = 'GUI_TextArea';
+    prLocTags.rows = 3;
+    
+    const div_prAnnotator = document.createElement('DIV');
+    const {prAnnotator} = this;
+    const prAnnotatorLabel = document.createElement('label');
+   // const Annotator = document.createElement("H3");
+    //Annotator.appendChild(document.createTextNode("Annotator"));
+    prAnnotatorLabel.className = 'neuroglancer-Proofread-homogeneous';
+    prAnnotatorLabel.textContent = 'Annotator    ';
+    prAnnotatorLabel.appendChild(prAnnotator);
+    div_prAnnotator.appendChild(linebreak);
+    div_prAnnotator.appendChild(linebreak);
+    //div_prAnnotator.appendChild(Annotator);
+    div_prAnnotator.appendChild(prAnnotatorLabel);
+    
+    element.appendChild(div_prAnnotator);
+    this.registerDisposer(a.changed.add(() => this.updateView()));
+    this.registerDisposer(this.visibility.changed.add(() => this.updateView()));
+    prAnnotator.addEventListener('save', () => this.updateModel());
+    prAnnotator.addEventListener('blur', () => this.updateModel());
+    prAnnotator.title = 'GUI_TextArea';
+    prAnnotator.rows = 1;
+    
+    /*const resetButton = document.createElement('button');
     resetButton.textContent = 'Reset to Empty';
-    resetButton.addEventListener('click', () => this.transform.reset());
-    element.appendChild(resetButton);
+    resetButton.addEventListener('click', () => this.reset());
+    element.appendChild(resetButton);*/
     this.updateView();
   }
+
+  /*private reset(){
+    this.transform.reset();
+    this.t.reset();
+  }*/
 
   private updateView() {
     if (!this.visible) {
       return;
     }
-    this.textArea.value = '' + this.transform._value;
+    this.prNeuronName.value = '' + this.transform._value;
+    this.prTags.value = '' + this.t._value;
+    this.prLocTags.value = '' + this.lt._value;
+    this.prAnnotator.value = '' + this.a._value;
   }
 
   private updateModel() {
     try
     {
-    this.transform._value=this.textArea.value;
+    this.transform._value=this.prNeuronName.value;
+    this.lt._value=this.prLocTags.value;
+    this.t._value=this.prTags.value;
+    this.a._value=this.prAnnotator.value;
+
+    this.t.changed.dispatch();
+    this.lt.changed.dispatch();
+    this.a.changed.dispatch();
     this.transform.changed.dispatch();
     }catch{
       this.updateView();
